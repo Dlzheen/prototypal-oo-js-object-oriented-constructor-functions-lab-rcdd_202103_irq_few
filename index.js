57c520df8060b42a64edf9133e912c9e3a2dde66
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-06-15 20:40:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
-function Scooter(homeState, training) {
-    this.homeState= homeState;
-    this.training =training;
-    this.sayHello = function() {
-      console.log(`${this.year},${this.color},${this.model}`);
-          };
-                    }
-      let samson = new Scooter(2015, "red", "swoosh");
-                        samson.Scooter();
-               function Driver(name, age,experience) {
-               this.name= name;
-                 this.age =age;
-                      this.experience = experience;
-                                  this.sayHello = function() {
-                                            console.log(`${this.name},${this.age},${this.experience}`);
-                                };
-                           }
-                    let allison = new Driver("Allison", "16 years", "2 years");
-                              allison.Driver();
-                     function PickupLocation (address,city) {
-               this.address= address;
-              this.city =city;
-                   this.sayHello = function() {
-                          console.log(`${this.address},${ this.city}`);
-                     };
-                    }
-             let poshHotel = new PickupLocation("123 Broadway","New York City");
-           poshHotel.PickupLocation();
+function Scooter(year, color, model) {
+            this.year = year;
+                this.color = color;
+                    this.model = model
+            }
+              function Driver(name, age, experience) {
+                         this.name = name;
+                                  this.age = age;
+                                     this.experience = experience
+            }
+                          function PickupLocation(address, city) {
+                                 this.address = address;
+                                   this.city = city;
+                     }
